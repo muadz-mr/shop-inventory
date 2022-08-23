28b04c35716d952aee82221630011eace7ea58ef
Mu'adz Mohd Rosli <muadz.mrosli@gmail.com>
2022-08-23 15:07:35 +0800

    feat: create product category model, migration, and table seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(CurrenciesTableSeeder::class);
+        $this->call(ProductCategoriesTableSeeder::class);
     }
 }
