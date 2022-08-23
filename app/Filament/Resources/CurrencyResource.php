cfef8c149a29d72db83738030f7728d3d59aa12a
Mu'adz Mohd Rosli <muadz.mrosli@gmail.com>
2022-08-23 17:25:54 +0800

    feat: create resources for currency model

---
+++ app/Filament/Resources/CurrencyResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CurrencyResource\Pages;
+use App\Filament\Resources\CurrencyResource\Pages\CreateCurrency;
+use App\Filament\Resources\CurrencyResource\Pages\EditCurrency;
+use App\Filament\Resources\CurrencyResource\Pages\ListCurrencies;
+use App\Filament\Resources\CurrencyResource\Pages\ViewCurrency;
+use App\Filament\Resources\CurrencyResource\RelationManagers;
+use App\Models\Currency;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class CurrencyResource extends Resource
+{
+    protected static ?string $model = Currency::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                static::getNameFormField(),
+                static::getSlugFormField(),
+                TextInput::make('code')->required()->maxLength(5)->autocomplete('off'),
+                TextInput::make('symbol')->required()->maxLength(10)->autocomplete('off'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('code')->searchable(),
+                TextColumn::make('symbol'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListCurrencies::route('/'),
+            'create' => CreateCurrency::route('/create'),
+            'view' => ViewCurrency::route('/{record}'),
+            'edit' => EditCurrency::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNameFormField(): TextInput
+    {
+        return TextInput::make('name')
+            ->required()
+            ->autocomplete('off')
+            ->reactive()
+            ->afterStateUpdated(fn ($state, callable $set) => $set('slug', Str::slug($state)));
+    }
+
+    public static function getSlugFormField(): TextInput
+    {
+        return TextInput::make('slug')
+            ->disabled()
+            ->required()
+            ->autocomplete('off')
+            ->unique(Currency::class, 'slug', fn ($record) => $record);
+    }
+}
